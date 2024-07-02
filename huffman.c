766323afdb797e9acdb8c660c1f16af5f71d95ca
phsmendonca <pedro.h.mendonca@edu.ufes.br>
2024-07-02 15:44:27 -0300

    comecando

---
+++ huffman.c
@@ -0,0 +1,15 @@
+//em ordem crescente (oq menos pro que mais apareceu, um nó pra cada pra cada um)
+
+//transformar string em vetor de char
+//quicksort no vetor
+
+int main(){
+
+    int V[256];
+
+    //surfar tudo e criar a lista
+
+
+
+    return 0;
+}
